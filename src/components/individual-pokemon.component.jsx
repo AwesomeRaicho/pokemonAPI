8960d81eaba4c6d239aff0ebd2bd31c8c84d5ecf
Ricardo Araujo <ricardo.dev0188@gmail.com>
2023-02-13 18:32:16 -0800

    changed order to id

--- src/components/individual-pokemon.component.jsx
+++ src/components/individual-pokemon.component.jsx
@@ -11,7 +11,7 @@ export const IndividualPokemon = ({pokemon}) => {
         <div className='individual-pokemon'>
             <img className='pokemon-sprite' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt="" />
             <p className='pokemon-name'>{pokemon.name}</p>
-            <div className='pokemon-number'>{pokemon.order}</div>
+            <div className='pokemon-number'>{pokemon.id}</div>
         </div>
   )
 }
